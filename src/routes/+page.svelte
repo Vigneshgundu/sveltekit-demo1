337829e522c72aa054ca09635ac19880f792a08a
Vigneshgundu <128591244+Vigneshgundu@users.noreply.github.com>
2024-07-13 22:32:07 +0530

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
+
 <p  class=" hover:bg-violet-600 text-center   font-bold">welcome to sveltekit
 </p>
-=======
-<h1>welcome to sveltekit
-</h1>
->>>>>>> 6bcfd10bd301f7edc65fb85dc4e884a0e58e308c
+
